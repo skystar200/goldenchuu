c9d1978984e56eef1b5cdfa5c6b5bcbcd458ab17
skystar200 <skystar20095@gmail.com>
2024-02-06 15:17:08 +0900

    Dynamodb + booking-info

--- ticketing-web/config/redis_py.py
+++ ticketing-web/config/redis_py.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import redis
-
 from flask_cors import CORS
+from awsConfig import get_dynamodb_resource
 
 app= Flask(__name__)
 CORS(app)  
@@ -10,23 +10,74 @@ redis_host = "localhost"
 redis_port = 6379
 redis_password = "test123"
 
+dynamodb = get_dynamodb_resource()
 
-@app.route('/get-seat-data/<sector>', methods=['GET'])
-def get_seat_data(sector):
+table = dynamodb.Table('init-db')
+
+@app.route('/get-seat-data/<sector>/<id>', methods=['GET'])
+def get_seat_data(sector,id):
     try:
         # Redis 클라이언트 생성
         r = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
+
+        resp = table.get_item(
+            Key={
+                'users.id': int(id)
+            }
+        )
+
+        if 'Item' in resp:
+            return jsonify({"seat_id": -1})
 
         # 데이터 가져오기
         value = r.lpop(f"{sector}-sector")
         
         if value:
-            return jsonify({"data": value.decode("utf-8")})  # byte string을 문자열로 디코딩하여 반환
+            # Redis에서 가져온 데이터
+            seat_id = value.decode("utf-8")
+
+
+            # DynamoDB에 데이터 입력
+            putitem = {
+                'users.id': int(id),
+                'seat_id': seat_id
+            }
+
+            print(putitem)
+
+            table.put_item(
+                Item=putitem
+            )
+
+            return jsonify({"seat_id": seat_id})
         else:
-            return jsonify({"data": None})  # 데이터가 없는 경우
+            # return jsonify({"error": "예매 실패(매진)"}), 404   # 데이터가 null인경우
+            return jsonify({"seat_id": None})  # 데이터가 없는 경우
     
     except Exception as e:
         return jsonify({"error": str(e)})
+    
+@app.route('/get-booking-info/<id>', methods=['GET'])
+def get_booking_info(id):
+    try:
+        # DynamoDB에서 데이터 조회
+        response = table.get_item(
+            Key={
+                'users.id': int(id)
+            }
+        )
+
+        # 조회된 데이터가 있는 경우
+        if 'Item' in response:
+            seat_id = response['Item'].get('seat_id')
+            print(response)
+            return jsonify({"seat_id": seat_id}), 200
+            
+        else:
+            return jsonify({"error": "Booking info not found"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ticketing-web/index.js
+++ ticketing-web/index.js
@@ -20,7 +20,6 @@
   const jwt = require('jsonwebtoken');
   const LocalStrategy = require('passport-local').Strategy;
   const cookieParser = require('cookie-parser');
-  
 
   // EJS 템플릿 엔진 설정
   app.engine('ejs', ejs.renderFile);
@@ -52,44 +51,44 @@
   app.use(passport.session());
 
   // 사용자 인증을 위한 미들웨어 정의
-  function isAuthenticated(req, res, next) {
-    if (req.cookies && req.cookies.access_token) { // 쿠키가 존재하는지 확인
-      const accessToken = req.cookies.access_token;
+  // function isAuthenticated(req, res, next) {
+  //   if (req.cookies && req.cookies.access_token) { // 쿠키가 존재하는지 확인
+  //     const accessToken = req.cookies.access_token;
       
-      try {
-        // 토큰 검증
-        const decoded = jwt.verify(accessToken, 'test_key'); // 발급할 때 사용한 비밀 키와 동일해야 함
+  //     try {
+  //       // 토큰 검증
+  //       const decoded = jwt.verify(accessToken, 'test_key'); // 발급할 때 사용한 비밀 키와 동일해야 함
         
-        // 토큰에서 추출한 사용자 아이디
-        const userId = decoded.userId;
+  //       // 토큰에서 추출한 사용자 아이디
+  //       const userId = decoded.userId;
   
-        // 데이터베이스에서 해당 사용자 정보 가져오기
-        const query = 'SELECT * FROM users WHERE userId = ?';
-        connection.query(query, [userId], (err, results) => {
-          if (err) {
-            console.error('쿼리 오류:', err);
-            res.status(500).json({ success: false, message: '서버 오류가 발생했습니다.' });
-            return;
-          }
+  //       // 데이터베이스에서 해당 사용자 정보 가져오기
+  //       const query = 'SELECT * FROM users WHERE userId = ?';
+  //       connection.query(query, [userId], (err, results) => {
+  //         if (err) {
+  //           console.error('쿼리 오류:', err);
+  //           res.status(500).json({ success: false, message: '서버 오류가 발생했습니다.' });
+  //           return;
+  //         }
   
-          if (results.length > 0) {
-            // 사용자 정보가 일치하는 경우
-            req.user = results[0]; // 요청 객체에 사용자 정보 추가
-            next(); // 다음 미들웨어로 이동
-          } else {
-            // 사용자 정보가 일치하지 않는 경우
-            res.status(401).json({ success: false, message: '사용자 정보가 일치하지 않습니다.' });
-          }
-        });
-      } catch (error) {
-        // 토큰이 유효하지 않은 경우
-        res.status(401).json({ success: false, message: '유효하지 않은 토큰입니다.' });
+  //         if (results.length > 0) {
+  //           // 사용자 정보가 일치하는 경우
+  //           req.user = results[0]; // 요청 객체에 사용자 정보 추가
+  //           next(); // 다음 미들웨어로 이동
+  //         } else {
+  //           // 사용자 정보가 일치하지 않는 경우
+  //           res.status(401).json({ success: false, message: '사용자 정보가 일치하지 않습니다.' });
+  //         }
+  //       });
+  //     } catch (error) {
+  //       // 토큰이 유효하지 않은 경우
+  //       res.status(401).json({ success: false, message: '유효하지 않은 토큰입니다.' });
         
-      }
-    } else {
-      res.redirect('/login'); // 쿠키가 없는 경우 로그인 페이지로 리다이렉션
-    }
-  }
+  //     }
+  //   } else {
+  //     res.redirect('/login'); // 쿠키가 없는 경우 로그인 페이지로 리다이렉션
+  //   }
+  // }
 
   const cors = require('cors');
   app.use(cors());
@@ -107,63 +106,16 @@
     },
   }));
 
-  // app.get('/seats', (req, res) => {
-  //   res.render('seats', { pageTitle: '좌석' });
-  // });
-
-  // app.get('/seats', isAuthenticated, (req, res) => {
-  //   res.render('seats', { pageTitle: '좌석' });
-  // });
+  //좌석예매 관련
 
   app.get('/seats', (req, res) => {
     // 페이지 제목을 변수에 저장
     const pageTitle = "좌석";
     // 렌더링 시 pageTitle 변수를 전달
-    res.render('seats', { pageTitle });
-});
+    const user = req.session.user;
+    res.render('seats', { pageTitle, user: user });
 
-
-
-  // 정적 파일 서빙
-  // app.use('/public', express.static(path.join(__dirname, 'public')));
-  io.on('connection', (socket) => {
-
-    // 클라이언트에 좌석 정보 전송
-    socket.emit('seats', seats);
-
-    // 좌석 선택 이벤트 처리
-    socket.on('seatSelected', (seatId) => {
-      const selectedSeat = seats.find((seat) => seat.id === seatId);
-
-      if (selectedSeat && selectedSeat.status === 'available') {
-        selectedSeat.status = 'selected';
-        selectedSeats.add(seatId);
-
-        // 클라이언트에 선택된 좌석 정보 전송
-        io.emit('seats', seats);
-      }
-    }); 
-
-    //예매 완료 이벤트 처리
-    socket.on('checkout', () => {
-      // 여기에서 결제 로직을 처리할 수 있습니다.
-
-      // 판매 완료된 좌석 상태 업데이트
-      selectedSeats.forEach((seatId) => {
-        const soldSeat = seats.find((seat) => seat.id === seatId);
-        if (soldSeat) {
-          soldSeat.status = 'sold';
-        }
-      });
-
-      // 클라이언트에 업데이트된 좌석 정보 전송
-      io.emit('seats', seats);
-
-      // 선택된 좌석 초기화
-      selectedSeats.clear();
-    });
   });
-
 
   // 로그인 페이지 렌더링
   app.get('/login', (req, res) => {
@@ -178,6 +130,54 @@
 
 //로그인
   app.post('/login', (req, res) => {
+    const { userId, password } = req.body;
+
+    // 데이터베이스에서 사용자 정보 확인 및 로그인 처리
+    const query = 'SELECT * FROM users WHERE userId = ? AND password = ?';
+
+    connection.query(query, [userId, password], (err, results) => {
+      if (err) {
+        console.error('쿼리 오류:', err);
+        res.json({ success: false, message: '로그인 중 오류가 발생했습니다.' });
+        return;
+      }
+
+      if (results.length > 0) {
+        // 세션 객체 초기화
+        req.session = req.session || {};
+        // 로그인 성공 시 사용자 정보를 세션에 저장
+        req.session.user = {
+          id: results[0].id,
+          userId: results[0].userId,
+          // password: results[0].password,
+          email: results[0].email,
+          userName: results[0].userName,
+          phone: results[0].phone
+
+        };
+        //JWT 토큰 발급
+        const accessToken = jwt.sign(req.session.user, 'test_key'); // 토큰에 담길 정보와 비밀 키 전달
+        // 발급한 토큰을 쿠키에 저장
+        res.cookie('access_token', accessToken, { httpOnly: true }); // httpOnly 옵션은 JavaScript로 쿠키에 접근할 수 없도록 설정
+        res.json({ success: true, message: '로그인이 완료되었습니다.' });
+        
+      } else {
+        res.json({ success: false, message: '사용자명 또는 비밀번호가 올바르지 않습니다.' });
+      }
+
+    });
+  });
+
+
+  // 로그인 라우트에 Passport를 사용하여 로그인 인증 처리
+  app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home', // 로그인 성공 시 이동할 경로
+    failureRedirect: '/login',     // 로그인 실패 시 이동할 경로
+    failureFlash: true             // 인증 실패 시 플래시 메시지 사용 여부
+  }));
+
+  // 예매하기 버튼으로 login 눌렀을 경우 성공시 /seats로 리다이렉션
+  app.post('/events-login', (req, res) => {
     const { userId, password } = req.body;
 
     // 데이터베이스에서 사용자 정보 확인 및 로그인 처리
@@ -215,10 +215,8 @@
     });
   });
 
-
-  // 로그인 라우트에 Passport를 사용하여 로그인 인증 처리
-  app.post('/login', passport.authenticate('local', {
-    successRedirect: '/home', // 로그인 성공 시 이동할 경로
+  app.post('/events-login', passport.authenticate('local', {
+    successRedirect: '/seats', // 로그인 성공 시 이동할 경로
     failureRedirect: '/login',     // 로그인 실패 시 이동할 경로
     failureFlash: true             // 인증 실패 시 플래시 메시지 사용 여부
   }));
@@ -276,10 +274,13 @@
 
   // 이벤트 디테일
   app.get('/events', (req, res) => {
-    res.render('events-1', { pageTitle: 'Details' });
+    // res.render('events-1', { pageTitle: 'Details', user: req.session.user  });
+  
+    const pageTitle = "Details";
+    // 렌더링 시 pageTitle 변수를 전달
+    const user = req.session.user;
+    res.render('events-1', { pageTitle, user: user });
   });
-
-
 
   app.get('/', (req, res) => {
     // 렌더링할 데이터를 객체로 전달
@@ -301,15 +302,16 @@
 
   app.post('/pop-seat', async (req, res) => {
     try {
+      const id = req.session.id;
       const selectedSeat = req.body.seat; // 클라이언트에서 선택한 좌석
       const sector = selectedSeat.split('-')[0]; // 좌석에서 섹터 부분 추출
-      const response = await axios.get(`http://localhost:5000/get-seat-data/${sector}`);
+      const response = await axios.get(`http://localhost:5000/get-seat-data/${sector}/${id}`);
       
-      if (response.status === 200) {
-        const data = response.data;
-        console.log('Data from Flask Server:', data);
+      if (response.status === 200) {    
+        const seat_id = response.data.seat_id;
+        console.log('Data from Flask Server:', seat_id);
         // 여기서 좌석 데이터를 사용하여 다른 작업을 수행할 수 있습니다.
-        res.status(200).json(data);
+        res.status(200).json({ seat_id });
       } else {
         console.error('Failed to get seat data from Flask Server.');
         res.status(500).send('Failed to get seat data from Flask Server.');
@@ -320,17 +322,59 @@
     }
   });
 
-  app.get('/get-seat-data/:seat', async (req, res) => {
+  app.get('/get-seat-data/:seat/:id', async (req, res) => {
     try {
       const seat = req.params.seat;
-      const response = await axios.get(`http://localhost:5000/get-seat-data/${seat}`);
-      const data = response.data;
-      res.status(200).json(data);
+      const id = req.params.id;
+      const response = await axios.get(`http://localhost:5000/get-seat-data/${seat}/${id}`);
+      const seat_id= response.data.seat_id;
+      res.status(200).json({ seat_id });
     } catch (error) {
       console.error('Error fetching seat data:', error);
       res.status(500).send('Error fetching seat data.');
     }
   });
+  // app.post('/show-booking-info',async(req, res)=>{
+  //   try {
+  //     const id = req.session.id;
+  //     const response = await axios.get(`http://localhost:5000/get-booking-info/${id}`);
+      
+  //     if (response.status === 200) {    
+  //       const seat_id = response.data.seat_id;
+  //       res.status(200).json({ seat_id });
+  //     } else {
+  //       console.error('Failed to get seat data from Flask Server.');
+  //       res.status(500).send('Failed to get seat data from Flask Server.');
+  //     }
+  //   } catch (error) {
+  //     console.error('Error sending get-seat-data request to Flask Server:', error);
+  //     res.status(500).send('Error sending get-seat-data request to Flask Server.');
+  //   }
+
+  // })
+
+
+  // 프로필-예매조회
+  app.get('/get-booking-info/:id', async (req, res) =>{
+    try{
+      const id = req.params.id;
+      const dynamoDBresponse = await axios.get(`http://localhost:5000/get-booking-info/${id}`);
+      const seat_id = dynamoDBresponse.data.seat_id;
+      res.status(200).json({ seat_id });
+
+    }catch (error) {
+      // 에러가 발생하면 클라이언트에게 에러 메시지를 응답
+      res.status(500).json({ error: error.message });
+
+  }
+
+  });
+  
+
+
+
+
+
 
   app.get('/bookTicket', (req, res) => {
     const user = req.session.user;
@@ -339,6 +383,7 @@
 
     }else {
       res.redirect('/login');
+      
     }
   });
 
@@ -353,7 +398,12 @@
         // 세션에 사용자 정보가 없으면 로그인 페이지로 리디렉션
         res.redirect('/login');
     }
-});
+  });
+  app.get('/booking-success', (req, res) => {
+    const user = req.session.user;
+    res.render('booking-success',{user}); // booking-success.ejs 파일을 렌더링하여 클라이언트에게 전송
+    
+  });
 
  
 
