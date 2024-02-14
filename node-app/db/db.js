51cfd38ffd4f12299ac78feb35bb0197733940a2
BYEOL <80840476+skystar200@users.noreply.github.com>
2024-02-14 17:24:41 +0900

    Update db.js

--- node-app/db/db.js
+++ node-app/db/db.js
@@ -4,10 +4,7 @@ const mysql = require('mysql');
 const util = require('util');
 //RDS 연동
 const dbInfo = {
-    host: 'init-rds.c3oaiws8kipy.ap-northeast-2.rds.amazonaws.com',
-    user: 'admin',
-    password: 'test1234',
-    database: 'init_db',
+
 };
 
 // connection 변수 선언 및 초기화
