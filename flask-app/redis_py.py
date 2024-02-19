c1ca86b5723e71fb766cf9fa26e2ec9ce18ba403
skystar200 <skystar20095@gmail.com>
2024-02-19 10:24:11 +0900

    redis_endpoint

--- flask-app/redis_py.py
+++ flask-app/redis_py.py
@@ -7,7 +7,7 @@ app= Flask(__name__)
 CORS(app, supports_credentials=True)  
 
 
-redis_host = "init-test-ro.tfsu7r.ng.0001.apn2.cache.amazonaws.com"
+redis_host = "init-redis-cluster.tfsu7r.ng.0001.apn2.cache.amazonaws.com"
 redis_port = 6379
 
 # elasticache_endpoint = 'init-test-ro.tfsu7r.ng.0001.apn2.cache.amazonaws.com'
