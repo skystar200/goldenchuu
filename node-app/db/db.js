39cf14b2f94ce7c21d55a0a9679b9b532ecd4dc9
skystar200 <skystar20095@gmail.com>
2024-03-07 09:26:45 +0900

    modify db.js

--- node-app/db/db.js
+++ node-app/db/db.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 const util = require('util');
 //RDS 연동
 const dbInfo = {
-    host: 'init-mysql.czuy8ksq2pce.ap-northeast-2.rds.amazonaws.com',
+    host: 'proxy-init-wouldyou.proxy-czuy8ksq2pce.ap-northeast-2.rds.amazonaws.com',
     user: 'admin',
     password: 'test1234',
-    database: 'init__rds',
+    database: 'init_wouldyou',
 };
 
 // connection 변수 선언 및 초기화
